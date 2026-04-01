386aefa306b26080ba1484e5ace77de7c3abb17a
Husameldeen <mhusam87@gmail.com>
2026-04-01 06:27:27 +0200

    ready for deployment

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,101 +1,106 @@
-import {Component, useState} from 'react';
+import { useState } from "react";
 
-function SignUp({ loadUser, onRouteChange}) {
-    
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         email: '',
-    //         password: '',
-    //         name: ''
-    //     }
-    // }
+function SignUp({ loadUser, onRouteChange }) {
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //         email: '',
+  //         password: '',
+  //         name: ''
+  //     }
+  // }
 
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-    const onEmailChange = (event) => {
-        setEmail(event.target.value)
-    }
+  const onEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
 
-    const onPasswordChange = (event) => {
-        setPassword(event.target.value)
-    }
+  const onPasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-    const onNameChange = (event) => {
-        setName(event.target.value)
-    }
+  const onNameChange = (event) => {
+    setName(event.target.value);
+  };
 
-    const onSubmitSignUp = () => {
-        fetch('https://smart-brain-backend-phi.vercel.app/signup', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                email: email,
-                password: password,
-                name: name
-            })
-        })
-        .then(res => res.json())
-        .then(user => {
-            if (user.id) {
-                //console.log(user.name)
-                loadUser(user);
-                onRouteChange('home');
-            }
-        })
-    }
+  const onSubmitSignUp = () => {
+    fetch("https://smart-brain-backend-phi.vercel.app/signup", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        name: name,
+      }),
+    })
+      .then((res) => res.json())
+      .then((user) => {
+        if (user.id) {
+          //console.log(user.name)
+          loadUser(user);
+          onRouteChange("home");
+        }
+      });
+  };
 
-    return (
-        <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-            <div className="pa4 black-80">
-                <div className="measure">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="f1 fw6 ph0 mh0">Sign Up</legend>
-                    <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                        <input 
-                            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="text" 
-                            name="name"  
-                            id="name"
-                            onChange={onNameChange}
-                        />
-                    </div>
-                    <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                        <input 
-                            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="email" 
-                            name="email-address"  
-                            id="email-address" 
-                            onChange={onEmailChange}
-                        />
-                    </div>
-                    <div className="mv3">
-                        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                        <input 
-                            className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="password" 
-                            name="password"  
-                            id="password" 
-                            onChange={onPasswordChange}
-                        />
-                    </div>
-                    </fieldset>
-                    <div className="">
-                    <input 
-                        onClick={onSubmitSignUp}
-                        className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                        type="submit" 
-                        value="Sign Up" 
-                    />
-                    </div>
-                </div>
+  return (
+    <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <div className="pa4 black-80">
+        <div className="measure">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0">Sign Up</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="text"
+                name="name"
+                id="name"
+                onChange={onNameChange}
+              />
             </div>
-        </article>
-    )
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Email
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+                onChange={onEmailChange}
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+                onChange={onPasswordChange}
+              />
+            </div>
+          </fieldset>
+          <div className="">
+            <input
+              onClick={onSubmitSignUp}
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Sign Up"
+            />
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 }
 
 export default SignUp;
